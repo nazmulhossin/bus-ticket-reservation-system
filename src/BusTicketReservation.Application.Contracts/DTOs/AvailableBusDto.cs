10279ae313f7b345519de96ed6086d33b5912b69
nazmul-hossin <mdnazmulhossin999@gmail.com>
2025-10-26 13:12:43 +0600

    update search avaiable buses response

--- src/BusTicketReservation.Application.Contracts/DTOs/AvailableBusDto.cs
+++ src/BusTicketReservation.Application.Contracts/DTOs/AvailableBusDto.cs
@@ -6,8 +6,8 @@
         public string CompanyName { get; set; } = string.Empty;
         public string BusName { get; set; } = string.Empty;
         public string BusType { get; set; } = string.Empty;
-        public string StartTime { get; set; } = string.Empty;
-        public string ArrivalTime { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
         public int SeatsLeft { get; set; }
         public int Price { get; set; }
         public string RouteName { get; set; } = string.Empty;
