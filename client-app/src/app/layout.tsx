2fda7305905791470354834bf893a919b458ea44
nazmul-hossin <mdnazmulhossin999@gmail.com>
2025-10-26 13:57:06 +0600

    setup frontend

---
+++ client-app/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "GoBus",
+    template: "%s - GoBus",
+  },
+  description: "GoBus is a smart and efficient Bus Ticket Reservation System designed to simplify online ticket booking, seat selection, and route management.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client-app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      Bus Ticket Reservation System
+    </div>
+  );
+}
